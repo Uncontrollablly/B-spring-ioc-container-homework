8ec9142206548d11abfd1eea372f0045eb7c0a76
Yuqi_Wang <yuqi.wang@thoughtworks.com>
2020-06-19 00:28:09 +0800

    finish practice2 with two methods

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,21 +1,28 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-    private final GreetingService greetingService;
+//    private final GreetingService greetingService;
 
-    @Autowired
-    public GreetingController(GreetingService greetingService) {
-        this.greetingService = greetingService;
+    @Lookup
+    public GreetingService getPrototypeBean() {
+        return null;
     }
+
+//    @Autowired
+//    public GreetingController(GreetingService greetingService) {
+//        this.greetingService = greetingService;
+//    }
 
     @GetMapping("/greet")
     public String greet() {
+        GreetingService greetingService = getPrototypeBean();
         return greetingService.sayHi();
     }
 
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(value = "prototype")
 public class GreetingService {
+
+    public GreetingService() {
+        System.out.println("create GreetingService®");
+    }
 
     String sayHi() {
         return "hello world";
