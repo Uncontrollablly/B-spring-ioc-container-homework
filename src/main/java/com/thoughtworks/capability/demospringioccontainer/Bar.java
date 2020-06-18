3529c1b2055f8e6ae66ae3a5505d2e889fbc564c
Yuqi_Wang <yuqi.wang@thoughtworks.com>
2020-06-19 00:35:00 +0800

    finish practice3 use three methods

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,13 +1,23 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Bar {
 
+    @Autowired
     private Foo foo;
 
-    public Bar(Foo foo) {
-        this.foo = foo;
+//    public Bar(@Lazy Foo foo) {
+//        this.foo = foo;
+//    }
+
+    @PostConstruct
+    public void init() {
+        foo.setBar(this);
     }
 }
